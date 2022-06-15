a2a91ad25e2f84af52df528f174144e9521f7bb6
hotwatermorning <hotwatermorning@gmail.com>
2022-06-16 00:54:15 +0900

    Enable running pitch detector.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { setupAudio } from "./setupAudio";
+
+function PitchReadout({ running, latestPitch }) {
+  return (
+    <div className="Pitch-readout">
+      {latestPitch
+        ? `Latest pitch: ${latestPitch.toFixed(1)} Hz`
+        : running
+        ? "Listening..."
+        : "Paused"}
+    </div>
+  );
+}
+
+function AudioRecorderControl() {
+  // Ensure the latest state of the audio module is reflected in the UI
+  // by defining some variables (and a setter function for updating them)
+  // that are managed by React, passing their initial values to useState.
+
+  // 1. audio is the object returned from the initial audio setup that
+  //    will be used to start/stop the audio based on user input. While
+  //    this is initialized once in our simple application, it is good
+  //    practice to let React know about any state that _could_ change
+  //    again.
+  const [audio, setAudio] = React.useState(undefined);
+
+  // 2. running holds whether the application is currently recording and
+  //    processing audio and is used to provide button text (Start vs Stop).
+  const [running, setRunning] = React.useState(false);
+
+  // 3. latestPitch holds the latest detected pitch to be displayed in
+  //    the UI.
+  const [latestPitch, setLatestPitch] = React.useState(undefined);
+
+  // Initial state. Initialize the web audio once a user gesture on the page
+  // has been registered.
+  if (!audio) {
+    return (
+      <button
+        onClick={async () => {
+          setAudio(await setupAudio(setLatestPitch));
+          setRunning(true);
+        }}
+      >
+        Start listening
+      </button>
+    );
+  }
+
+  // Audio already initialized. Suspend / resume based on its current state.
+  const { context } = audio;
+  return (
+    <div>
+      <button
+        onClick={async () => {
+          if (running) {
+            await context.suspend();
+            setRunning(context.state === "running");
+          } else {
+            await context.resume();
+            setRunning(context.state === "running");
+          }
+        }}
+        disabled={context.state !== "running" && context.state !== "suspended"}
+      >
+        {running ? "Pause" : "Resume"}
+      </button>
+      <PitchReadout running={running} latestPitch={latestPitch} />
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Wasm Audio Tutorial
       </header>
+      <div className="App-content">
+        <AudioRecorderControl />
+      </div>
     </div>
   );
 }
---
+++ wasm-audio/src/lib.rs
@@ -0,0 +1,55 @@
+use pitch_detection::{McLeodDetector, PitchDetector};
+use wasm_bindgen::prelude::*;
+mod utils;
+
+#[wasm_bindgen]
+pub struct WasmPitchDetector {
+  sample_rate: usize,
+  fft_size: usize,
+  detector: McLeodDetector<f32>,
+}
+
+#[wasm_bindgen]
+impl WasmPitchDetector {
+  pub fn new(sample_rate: usize, fft_size: usize) -> WasmPitchDetector {
+    utils::set_panic_hook();
+
+    let fft_pad = fft_size / 2;
+
+    WasmPitchDetector {
+      sample_rate,
+      fft_size,
+      detector: McLeodDetector::<f32>::new(fft_size, fft_pad),
+    }
+  }
+
+  pub fn detect_pitch(&mut self, audio_samples: Vec<f32>) -> f32 {
+    if audio_samples.len() < self.fft_size {
+      panic!("Insufficient samples passed to detect_pitch(). Expected an array containing {} elements but got {}", self.fft_size, audio_samples.len());
+    }
+
+    // Include only notes that exceed a power threshold which relates to the
+    // amplitude of frequencies in the signal. Use the suggested default
+    // value of 5.0 from the library.
+    const POWER_THRESHOLD: f32 = 5.0;
+
+    // The clarity measure describes how coherent the sound of a note is. For
+    // example, the background sound in a crowded room would typically be would
+    // have low clarity and a ringing tuning fork would have high clarity.
+    // This threshold is used to accept detect notes that are clear enough
+    // (valid values are in the range 0-1).
+    const CLARITY_THRESHOLD: f32 = 0.6;
+
+    let optional_pitch = self.detector.get_pitch(
+      &audio_samples,
+      self.sample_rate,
+      POWER_THRESHOLD,
+      CLARITY_THRESHOLD,
+    );
+
+    match optional_pitch {
+      Some(pitch) => pitch.frequency,
+      None => 0.0,
+    }
+  }
+}
